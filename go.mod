97935494d99b4857c6d79f5c34ab1f100026896e
kavish.punchoo <kavish.punchoo@external.appspace.com>
2021-09-28 11:52:29 +0800

    added initial requests to influxdb and jenkins

---
+++ go.mod
@@ -0,0 +1,5 @@
+module jenkins-pipeline-telegraf
+
+go 1.16
+
+require github.com/influxdata/influxdb-client-go/v2 v2.5.1 // indirect
---
+++ structs.go
@@ -0,0 +1,38 @@
+package main
+
+type PipelineData []struct {
+	Links               Links    `json:"_links,omitempty"`
+	ID                  string   `json:"id"`
+	Name                string   `json:"name"`
+	Status              string   `json:"status"`
+	StartTimeMillis     int64    `json:"startTimeMillis"`
+	EndTimeMillis       int64    `json:"endTimeMillis"`
+	DurationMillis      int      `json:"durationMillis"`
+	QueueDurationMillis int      `json:"queueDurationMillis"`
+	PauseDurationMillis int      `json:"pauseDurationMillis"`
+	Stages              []Stages `json:"stages"`
+}
+
+type Self struct {
+	Href string `json:"href"`
+}
+
+type Links struct {
+	Self Self `json:"self"`
+}
+
+type Stages struct {
+	Links               Links         `json:"_links"`
+	ID                  string        `json:"id"`
+	Name                string        `json:"name"`
+	ExecNode            string        `json:"execNode"`
+	Status              string        `json:"status"`
+	StartTimeMillis     int64         `json:"startTimeMillis"`
+	DurationMillis      int           `json:"durationMillis"`
+	PauseDurationMillis int           `json:"pauseDurationMillis"`
+	StageFlowNodes      []interface{} `json:"stageFlowNodes"`
+}
+
+type Changesets struct {
+	Href string `json:"href"`
+}
